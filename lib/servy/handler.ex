eecfa33522b66f444887c2e1dfc504c6cbb54fc7
kamalogudah <mcpaul2058@gmail.com>
2018-08-21 18:04:58 +0300

    Using pattern matching in place of case statemnts

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -63,18 +63,31 @@ defmodule Servy.Handler do
     file =
       Path.expand("../../pages", __DIR__)
       |> Path.join("about.html")
-    case File.read(file) do
-      {:ok, content} ->
-        %{ conv | status: 200, resp_body: content }
+      |> File.read
+      |> handle_file(conv)
+    # case File.read(file) do
+    #   {:ok, content} ->
+    #     %{ conv | status: 200, resp_body: content }
 
-      {:error, enoent } ->
-        %{ conv | status: 400, resp_body: "File not found " }
+    #   {:error, enoent } ->
+    #     %{ conv | status: 400, resp_body: "File not found " }
 
-      {:error, reason } ->
-        %{ conv | status: 500, resp_body: "File error: #{reason} " }
-    end
+    #   {:error, reason } ->
+    #     %{ conv | status: 500, resp_body: "File error: #{reason} " }
+    # end
   end
 
+  def handle_file({:ok, content}, conv) do
+    %{ conv | status: 200, resp_body: content }
+  end
+
+  def handle_file({:error, enoent }, conv) do
+    %{ conv | status: 400, resp_body: "File not found " }
+  end
+
+  def handle_file({:error, reason }, conv) do
+    %{ conv | status: 500, resp_body: "File error: #{reason} " }
+  end
   def route(%{ path: path} = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!" }
   end
