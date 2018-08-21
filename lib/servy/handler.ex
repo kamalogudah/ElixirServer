26d91f02def9a3990e89983ab2a5319d30673526
kamalogudah <mcpaul2058@gmail.com>
2018-08-21 17:56:48 +0300

    Better reading of file with path

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -60,7 +60,10 @@ defmodule Servy.Handler do
   end
 
   def route(%{ method: "GET", path: "/about"} = conv) do
-    case File.read("./lib/pages/about.html") do
+    file =
+      Path.expand("../../pages", __DIR__)
+      |> Path.join("about.html")
+    case File.read(file) do
       {:ok, content} ->
         %{ conv | status: 200, resp_body: content }
 
