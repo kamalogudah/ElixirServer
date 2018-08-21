80de8a4074efe20b910624970a0bf6d5086c50bf
kamalogudah <mcpaul2058@gmail.com>
2018-08-21 17:51:52 +0300

    Read a file

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -60,7 +60,16 @@ defmodule Servy.Handler do
   end
 
   def route(%{ method: "GET", path: "/about"} = conv) do
-    %{ conv | status: 200, resp_body: "contents of the file" }
+    case File.read("./lib/pages/about.html") do
+      {:ok, content} ->
+        %{ conv | status: 200, resp_body: content }
+
+      {:error, enoent } ->
+        %{ conv | status: 400, resp_body: "File not found " }
+
+      {:error, reason } ->
+        %{ conv | status: 500, resp_body: "File error: #{reason} " }
+    end
   end
 
   def route(%{ path: path} = conv) do
