a41d7bb4f67c09240e5b9c874188069246ad5135
kamalogudah <mcpaul2058@gmail.com>
2018-08-21 16:44:27 +0300

    Request Params and Status Codes

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -44,6 +44,10 @@ defmodule Servy.Handler do
     %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
   end
 
+  def route(conv, "GET", "/bears/" <> id) do
+    %{ conv | status: 200, resp_body: "Bear #{id}"}
+  end
+
   def route(conv, _method, path) do
     %{ conv | status: 404, resp_body: "No #{path} here!" }
   end
@@ -72,7 +76,7 @@ defmodule Servy.Handler do
 end
 
 request = """
-GET /bs HTTP/1.1
+GET /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
