8aa6825b16e3ee96f8f671f97faf2133fe6f050a
kamalogudah <mcpaul2058@gmail.com>
2018-08-21 16:40:04 +0300

    Dyanamic Status Codes

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -50,12 +50,23 @@ defmodule Servy.Handler do
 
   def format_response(conv) do
     """
-    HTTP/1.1 #{conv.status} OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 
 end
